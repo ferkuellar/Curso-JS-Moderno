7e5378b24d2a76c3d061171bbdb8f89a168ac3cd
Fernando Cuellar <fercuellar@gmail.com>
2023-03-13 19:59:38 -0600

    Inicio del tema de APIs

---
+++ 34-APIs-JS/js/01-app.js
@@ -0,0 +1,24 @@
+const notificarBtn = document.querySelector('#notificar');
+
+notificarBtn.addEventListener('click', () => {
+    Notification
+        .requestPermission()
+        .then( resultado => {
+            console.log('El resultado es', resultado);
+        });
+});
+
+const verNotificacion = document.querySelector('#verNotificacion');
+
+verNotificacion.addEventListener('click', () => {
+    if(Notification.permission === 'granted'){
+        const notificacion = new Notification('Esta es la notificacion', {
+            icon: 'img/ccj.png',
+            body: 'Codigo con Fernando, aprende con proyectos reales'
+        });
+
+        notificacion.onclick = function() {
+            window.open('http://www.google.com')
+        };
+    };
+});
