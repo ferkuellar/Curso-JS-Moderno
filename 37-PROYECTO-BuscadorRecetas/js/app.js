9980ed8b365a8db5abf743b07c201cd0fa15c8eb
Fernando Cuellar <fercuellar@gmail.com>
2023-03-23 16:45:27 -0600

    se seleccionaron las categorias

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,6 +1,7 @@
 function iniciarApp() {
 
-    const selectCategorias = document.querySelector('#categorias')
+    const selectCategorias = document.querySelector('#categorias');
+    selectCategorias.addEventListener('change', seleccionarCategoria);
 
     obtenerCategorias();
 
@@ -20,6 +21,12 @@ function iniciarApp() {
             option.textContent = strCategory;
             selectCategorias.appendChild(option);
         });
+    };
+
+    function seleccionarCategoria(e) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+        console.log(url)
     };
 };
 
