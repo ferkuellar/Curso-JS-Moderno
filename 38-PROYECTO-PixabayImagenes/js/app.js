a4c30b219a890c1765816584e4be2239efefa38d
Fernando Cuellar <fercuellar@gmail.com>
2023-04-25 19:44:17 -0600

    Se muestran los resultados de la pagina

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -44,11 +44,49 @@ function mostrarAlerta(mensaje){
 
 function buscarImagens(termino){
     const key = '35704188-a651f7e7dbf8bbaa9a4f3e9db';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado=>{
-            console.log(resultado);
-        })
+            mostrarImagenes(resultado.hits);
+        });
+};
+
+function mostrarImagenes(imagenes){
+    console.log(imagenes);
+
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imagenes y cosntruir el HTML
+    imagenes.forEach(imagen => {
+
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+
+            <div class="w-1/2 md:1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p>${likes} <span class="font-light"> Me Gusta </span> </p>
+                        <p>${views} <span class="font-light"> Veces vista </span> </p>
+
+                        <a 
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"    
+
+                            href="${largeImageURL}" target="_balnk" rel="noopener noreferrer"
+                        >
+                            Ver Imagen
+                        </a>
+                    </div>
+                <div class="bg-white">
+            </div>
+        `;
+
+    })
+
 }
